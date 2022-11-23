1955664a3273410f947f296890ce3785a3182bfe
i.shirinov <shirinovilyos6965@gmail.com>
2022-11-23 15:06:57 +0500

    №1 vazifa. Oddiy bo'lsa shartlar

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean a = true;
+        if (a) {
+            System.out.println(a);
+        }
     }
 }
